0590c085c3a07b3c1da42925e6276475d9db89ae
Sebastian Certuche <sebascertuche@gmail.com>
2025-05-18 02:14:32 -0600

    Format and make renderables static

--- include/renderable.hpp
+++ include/renderable.hpp
@@ -1,27 +1,36 @@
-#include "../include/libs.hpp"
+#include "libs.hpp"
 
-class renderable {
-public:
-  renderable(std::vector<renderable *> &my_vector, sf::Vector2f position,
-             int width, int height, std::string spritesheet_path,
-             int render_width = -1, int render_height = -1);
-  ~renderable() {}
-  void render(sf::RenderWindow &window);
-  void update_spritesheet_cords();
-  sf::Vector2f get_position();
+class renderable
+{
+   public:
+    renderable(sf::Vector2f position, int width, int height,
+               std::string spritesheet_path, int render_width = -1,
+               int render_height = -1);
+    ~renderable()
+    {
+    }
+    void render(sf::RenderWindow &window);
+    void update_spritesheet_cords();
+    sf::Vector2f get_position();
 
-  int x = 0;
-  int y = 0;
+    int x = 0;
+    int y = 0;
 
-protected:
-  int _width;
-  int _height;
-  int _render_width;
-  int _render_height;
-  int _render_x = 0;
-  int _render_y = 0;
-  int _direction = 1;
-  sf::Texture _texture;
-  sf::Sprite _sprite;
-  std::vector<renderable *> &_my_vector;
+    static const std::vector<renderable *> &getRenderables()
+    {
+        return renderables;
+    }
+
+   protected:
+    int _width;
+    int _height;
+    int _render_width;
+    int _render_height;
+    int _render_x = 0;
+    int _render_y = 0;
+    int _direction = 1;
+    sf::Texture _texture;
+    sf::Sprite _sprite;
+
+    static std::vector<renderable *> renderables;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,29 @@
-#include "../include/libs.hpp"
-#include "../include/renderable.hpp"
+#include "libs.hpp"
+#include "renderable.hpp"
 
-int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Clicker");
-  window.setFramerateLimit(60);
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Clicker");
+    window.setFramerateLimit(60);
 
-  std::vector<renderable *> renderable_vector;
+    renderable player(sf::Vector2f(100, 100), 33, 30, "assets/alpha/hand.png",
+                      33, 30);
 
-  renderable player(renderable_vector, sf::Vector2f(100, 100), 33, 30,
-                    "assets/alpha/hand.png", 33, 30);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
+        window.clear();
+
+        for (renderable *renderable : renderable::getRenderables()) {
+            renderable->render(window);
+        }
+
+        window.display();
     }
 
-    window.clear();
-
-    for (renderable *renderable : renderable_vector) {
-      renderable->render(window);
-    }
-
-    window.display();
-  }
-
-  return 0;
+    return 0;
 }
